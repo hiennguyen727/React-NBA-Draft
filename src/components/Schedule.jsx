1cd7a15df54ba2cef508602ab351760dba7127c7
hiennguyen727 <hien.d.nguyen1997@gmail.com>
2023-10-12 22:44:37 -0400

    fds

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import '../NBAGamesBar.css';
+import '../NbaGamesBar.css';
 
 class NBAGamesBar extends Component {
   constructor() {
